6edb2b2252091f74a7f6632b7aedf6d5e504cc04
DadianisBidza <kutxisdaxra@outlook.com>
2017-11-20 11:35:49 +0400

    Added TensorBoard logging, gave names to all layers, modified StandardFit to support transfer-learning for CUB-200-2011. Added VGG-Train.py.

--- LearnToPayAttention.py
+++ LearnToPayAttention.py
@@ -10,72 +10,72 @@ from keras.layers.core import Dense, Lambda, Activation, Flatten
 from keras.layers.convolutional import Conv2D
 from keras.layers.merge import Concatenate, Add
 from keras.layers.pooling import MaxPooling2D
-from keras.callbacks import LearningRateScheduler, ModelCheckpoint, LambdaCallback
+from keras.callbacks import LearningRateScheduler, ModelCheckpoint, LambdaCallback, TensorBoard
 
 
 class AttentionVGG:
-    def __init__(self, att='att1', gmode='concat', compatibilityfunction='pc', height=32, width=32, channels=3, outputclasses=100, weight_decay=0.005):
+    def __init__(self, att='att1', gmode='concat', compatibilityfunction='pc', datasetname="cifar100", height=32, width=32, channels=3, outputclasses=100, weight_decay=0.005):
         inp = Input(shape=(height, width, channels))
         regularizer = keras.regularizers.l2(weight_decay)
 
-        x = Conv2D(64, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizer)(inp)
-        x = Conv2D(64, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizer)(x)
+        x = Conv2D(64, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizer, name='conv1')(inp)
+        x = Conv2D(64, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizer, name='conv2')(x)
 
-        x = Conv2D(128, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizer)(x)
-        x = Conv2D(128, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizer)(x)
+        x = Conv2D(128, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizer, name='conv3')(x)
+        x = Conv2D(128, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizer, name='conv4')(x)
 
-        x = Conv2D(256, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizer)(x)
-        x = Conv2D(256, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizer)(x)
-        local1 = Conv2D(256, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizer)(x)  # batch*x*y*channel
-        x = MaxPooling2D((2, 2), strides=(2, 2))(local1)
+        x = Conv2D(256, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizer, name='conv5')(x)
+        x = Conv2D(256, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizer, name='conv6')(x)
+        local1 = Conv2D(256, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizer, name='conv7')(x)  # batch*x*y*channel
+        x = MaxPooling2D((2, 2), strides=(2, 2), name='pool1')(local1)
 
-        x = Conv2D(512, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizer)(x)
-        x = Conv2D(512, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizer)(x)
-        local2 = Conv2D(512, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizer)(x)
-        x = MaxPooling2D((2, 2), strides=(2, 2))(local2)
+        x = Conv2D(512, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizer, name='conv8')(x)
+        x = Conv2D(512, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizer, name='conv9')(x)
+        local2 = Conv2D(512, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizer, name='conv10')(x)
+        x = MaxPooling2D((2, 2), strides=(2, 2), name='pool2')(local2)
 
-        x = Conv2D(512, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizer)(x)
-        x = Conv2D(512, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizer)(x)
-        local3 = Conv2D(512, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizer)(x)
-        x = MaxPooling2D((2, 2), strides=(2, 2))(local3)
+        x = Conv2D(512, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizer, name='conv11')(x)
+        x = Conv2D(512, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizer, name='conv12')(x)
+        local3 = Conv2D(512, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizer, name='conv13')(x)
+        x = MaxPooling2D((2, 2), strides=(2, 2), name='pool3')(local3)
 
-        x = Conv2D(512, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizer)(x)
-        x = MaxPooling2D((2, 2), strides=(2, 2))(x)
-        x = Conv2D(512, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizer)(x)
-        x = MaxPooling2D((2, 2), strides=(2, 2))(x)
-        x = Flatten(name='flatten')(x)
-        g = Dense(512, activation='relu', kernel_regularizer=regularizer)(x)  # batch*512
+        x = Conv2D(512, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizer, name='conv14')(x)
+        x = MaxPooling2D((2, 2), strides=(2, 2), name='pool4')(x)
+        x = Conv2D(512, (3, 3), activation='relu', padding='same', kernel_regularizer=regularizer, name='conv15')(x)
+        x = MaxPooling2D((2, 2), strides=(2, 2), name='pool5')(x)
+        x = Flatten(name='pregflatten')(x)
+        g = Dense(512, activation='relu', kernel_regularizer=regularizer, name='globalg')(x)  # batch*512
 
-        l1 = Dense(512, kernel_regularizer=regularizer)(local1)  # batch*x*y*512
-        c1 = ParametrisedCompatibility(kernel_regularizer=regularizer)([l1, g])  # batch*x*y
+        l1 = Dense(512, kernel_regularizer=regularizer, name='l1connectordense')(local1)  # batch*x*y*512
+        c1 = ParametrisedCompatibility(kernel_regularizer=regularizer, name='cpc1')([l1, g])  # batch*x*y
         if compatibilityfunction == 'dp':
-            c1 = Lambda(lambda lam: K.squeeze(K.map_fn(lambda xy: K.dot(xy[0], xy[1]), elems=(lam[0], K.expand_dims(lam[1], -1)), dtype=tf.float32), 3))([l1, g])  # batch*x*y
-        flatc1 = Flatten()(c1)  # batch*xy
-        a1 = Activation('softmax')(flatc1)  # batch*xy
-        reshaped1 = Lambda(lambda la: K.map_fn(lambda lam: K.reshape(lam, [-1, 512]), elems=[la], dtype=tf.float32))(l1)  # batch*xy*512.
-        g1 = Lambda(lambda lam: K.squeeze(K.batch_dot(K.expand_dims(lam[0], 1), lam[1]), 1))([a1, reshaped1])  # batch*512.
+            c1 = Lambda(lambda lam: K.squeeze(K.map_fn(lambda xy: K.dot(xy[0], xy[1]), elems=(lam[0], K.expand_dims(lam[1], -1)), dtype=tf.float32), 3), name='cdp1')([l1, g])  # batch*x*y
+        flatc1 = Flatten(name='flatc1')(c1)  # batch*xy
+        a1 = Activation('softmax', name='softmax1')(flatc1)  # batch*xy
+        reshaped1 = Lambda(lambda la: K.map_fn(lambda lam: K.reshape(lam, [-1, 512]), elems=[la], dtype=tf.float32), name='reshape1')(l1)  # batch*xy*512.
+        g1 = Lambda(lambda lam: K.squeeze(K.batch_dot(K.expand_dims(lam[0], 1), lam[1]), 1), name='g1')([a1, reshaped1])  # batch*512.
 
         height = height//2
         width = width//2
         l2 = local2
-        c2 = ParametrisedCompatibility(kernel_regularizer=regularizer)([l2, g])
+        c2 = ParametrisedCompatibility(kernel_regularizer=regularizer, name='cpc2')([l2, g])
         if compatibilityfunction == 'dp':
-            c2 = Lambda(lambda lam: K.squeeze(K.map_fn(lambda xy: K.dot(xy[0], xy[1]), elems=(lam[0], K.expand_dims(lam[1], -1)), dtype=tf.float32), 3))([l2, g])
-        flatc2 = Flatten()(c2)
-        a2 = Activation('softmax')(flatc2)
-        reshaped2 = Lambda(lambda la: K.map_fn(lambda lam: K.reshape(lam, [-1, 512]), elems=[la], dtype=tf.float32))(l2)
-        g2 = Lambda(lambda lam: K.squeeze(K.batch_dot(K.expand_dims(lam[0], 1), lam[1]), 1))([a2, reshaped2])
+            c2 = Lambda(lambda lam: K.squeeze(K.map_fn(lambda xy: K.dot(xy[0], xy[1]), elems=(lam[0], K.expand_dims(lam[1], -1)), dtype=tf.float32), 3), name='cdp2')([l2, g])
+        flatc2 = Flatten(name='flatc2')(c2)
+        a2 = Activation('softmax', name='softmax2')(flatc2)
+        reshaped2 = Lambda(lambda la: K.map_fn(lambda lam: K.reshape(lam, [-1, 512]), elems=[la], dtype=tf.float32), name='reshape2')(l2)
+        g2 = Lambda(lambda lam: K.squeeze(K.batch_dot(K.expand_dims(lam[0], 1), lam[1]), 1), name='g2')([a2, reshaped2])
 
         height = height//2
         width = width//2
         l3 = local3
-        c3 = ParametrisedCompatibility(kernel_regularizer=regularizer)([l3, g])
+        c3 = ParametrisedCompatibility(kernel_regularizer=regularizer, name='cpc3')([l3, g])
         if compatibilityfunction == 'dp':
-            c3 = Lambda(lambda lam: K.squeeze(K.map_fn(lambda xy: K.dot(xy[0], xy[1]), elems=(lam[0], K.expand_dims(lam[1], -1)), dtype=tf.float32), 3))([l3, g])
-        flatc3 = Flatten()(c3)
-        a3 = Activation('softmax')(flatc3)
-        reshaped3 = Lambda(lambda la: K.map_fn(lambda lam: K.reshape(lam, [-1, 512]), elems=[la], dtype=tf.float32))(l3)
-        g3 = Lambda(lambda lam: K.squeeze(K.batch_dot(K.expand_dims(lam[0], 1), lam[1]), 1))([a3, reshaped3])
+            c3 = Lambda(lambda lam: K.squeeze(K.map_fn(lambda xy: K.dot(xy[0], xy[1]), elems=(lam[0], K.expand_dims(lam[1], -1)), dtype=tf.float32), 3), name='cdp3')([l3, g])
+        flatc3 = Flatten(name='flatc3')(c3)
+        a3 = Activation('softmax', name='softmax3')(flatc3)
+        reshaped3 = Lambda(lambda la: K.map_fn(lambda lam: K.reshape(lam, [-1, 512]), elems=[la], dtype=tf.float32), name='reshape3')(l3)
+        g3 = Lambda(lambda lam: K.squeeze(K.batch_dot(K.expand_dims(lam[0], 1), lam[1]), 1), name='g3')([a3, reshaped3])
 
         out = ''
         if gmode == 'concat':
@@ -86,23 +86,23 @@ class AttentionVGG:
                 glist.append(g2)
                 glist.append(g1)
             predictedG = g1
-            if att! = 'att1' and att != 'att':
-                predictedG = Concatenate(axis=1)(glist)
-            x = Dense(outputclasses, kernel_regularizer=regularizer)(predictedG)
-            out = Activation("softmax")(x)
+            if att != 'att1' and att != 'att':
+                predictedG = Concatenate(axis=1, name='ConcatG')(glist)
+            x = Dense(outputclasses, kernel_regularizer=regularizer, name=str(outputclasses)+'ConcatG')(predictedG)
+            out = Activation("softmax", name='concatsoftmaxout')(x)
         else:
-            gd3 = Dense(outputclasses, activation='softmax')(g3)
+            gd3 = Dense(outputclasses, activation='softmax', name=str(outputclasses)+'indepsoftmaxg3')(g3)
             if att == 'att' or att == 'att1':
                 out = gd3
             elif att == 'att2':
-                gd2 = Dense(outputclasses, activation='sotfmax', kernel_regularizer=regularizer)(g2)
-                out = Add()([gd3, gd2])
-                out = Lambda(lambda lam: lam/2)(out)
+                gd2 = Dense(outputclasses, activation='sotfmax', kernel_regularizer=regularizer, name=str(outputclasses)+'indepsoftmaxg2')(g2)
+                out = Add()([gd3, gd2], name='addg3g2')
+                out = Lambda(lambda lam: lam/2, name='2average')(out)
             else:
-                gd2 = Dense(outputclasses, activation='softmax', kernel_regularizer=regularizer)(g2)
-                gd1 = Dense(outputclasses, activation='softmax', kernel_regularizer=regularizer)(g1)
-                out = Add()([gd1, gd2, gd3])
-                out = Lambda(lambda lam: lam/3)(out)
+                gd2 = Dense(outputclasses, activation='softmax', kernel_regularizer=regularizer, name=str(outputclasses)+'indepsoftmaxg2')(g2)
+                gd1 = Dense(outputclasses, activation='softmax', kernel_regularizer=regularizer, name=str(outputclasses)+'indepsoftmaxg2')(g1)
+                out = Add()([gd1, gd2, gd3], name='addg3g2g1')
+                out = Lambda(lambda lam: lam/3, name='3average')(out)
 
         model = Model(inputs=inp, outputs=out)
         model.compile(optimizer=keras.optimizers.SGD(lr=1, momentum=0.9, decay=0.0000001, nesterov=False), loss='categorical_crossentropy', metrics=['accuracy'])
@@ -111,21 +111,46 @@ class AttentionVGG:
         self.name = name
         self.model = model
 
-    def StandardFit(self, datasetname, X, Y):
+    def StandardFit(self, X, Y, transfer=False):
+        scheduler = LearningRateScaler(25, 0.5)
         startingepoch = 0
-        pastepochs = list(map(int, [x.replace(".hdf5", "").replace(self.name+"-"+datasetname, "").replace(" ", "") for x in os.listdir("weights") if self.name+"-"+datasetname in x]))
+        pastepochs = list(map(int, [x.replace(".hdf5", "").replace(self.name+"-"+self.datasetname, "").replace(" ", "") for x in os.listdir("weights") if self.name+"-"+datasetname in x]))
         if pastepochs:
             if max(pastepochs) == 300:
-                print("Found completely trained weights for "+self.name+"-"+datasetname)
+                print("Found completely trained weights for "+self.name+"-"+self.datasetname)
                 return
-            self.model.load_weights("weights/"+self.name+"-"+datasetname+" "+str(max(pastepochs))+".hdf5")
+            self.model.load_weights("weights/"+self.name+"-"+self.datasetname+" "+str(max(pastepochs))+".hdf5")
             startingepoch = max(pastepochs)
-        scaler = LearningRateScaler(25, 0.5)
-        tboardcb = keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=0, batch_size=3, write_graph=True, write_grads=False, write_images=False, embeddings_freq=0, embeddings_layer_names=None, embeddings_metadata=None)
-        checkpoint = ModelCheckpoint("weights/"+self.name+"-"+datasetname+" {epoch}.hdf5", save_weights_only=True)
+        elif transfer:
+            self.model.load_weights("weights/"+self.name+"-cifar100 300.hdf5", by_name=True)
+            scheduler = LearningRateScheduler(transfer_schedule)
+        tboardcb = TensorBoard(log_dir='./logs', histogram_freq=0, batch_size=3, write_graph=True, write_grads=False, write_images=False, embeddings_freq=0, embeddings_layer_names=None, embeddings_metadata=None)
+        checkpoint = ModelCheckpoint("weights/"+self.name+"-"+self.datasetname+" {epoch}.hdf5", save_weights_only=True)
         epochprint = LambdaCallback(on_epoch_end=lambda epoch, logs: print("Passed epoch "+str(epoch)))
-        callbackslist = [scaler, checkpoint, epochprint, tboardcb]
-        self.model.fit(X, Y, 3, 3, callbacks=callbackslist, initial_epoch=startingepoch)
+        callbackslist = [scheduler, checkpoint, epochprint, tboardcb]
+        self.model.fit(X, Y, 128, 300, callbacks=callbackslist, initial_epoch=startingepoch)
+        return self.model
+
+    def transfer_schedule(epoch):
+            if epoch < 30:
+                return 0.1
+            if epoch < 60:
+                return 0.2
+            if epoch < 90:
+                return 0.4
+            if epoch < 120:
+                return 0.2
+            if epoch < 150:
+                return 0.1
+            if epoch < 180:
+                return 0.05
+            if epoch < 210:
+                return 0.025
+            if epoch < 240:
+                return 0.0125
+            if epoch < 270:
+                return 0.00625
+            return 0.003125
 
 
 class ParametrisedCompatibility(Layer):
@@ -162,7 +187,4 @@ class LearningRateScaler(keras.callbacks.Callback):
 
 
 if __name__ == "__main__":
-    a = AttentionVGG(att='att3', gmode='concat', compatibilityfunction='pc', height=32, width=32, channels=3, outputclasses=10)
-    X = np.ones((3, 32, 32, 3))
-    Y = 3*np.ones((3, 10))
-    a.StandardFit("randomset", X, Y)
+    testmodel = AttentionVGG(att='att3', gmode='concat', compatibilityfunction='pc', datasetname="randomset", height=32, width=32, channels=3, outputclasses=10)
---
+++ VGG-Train.py
@@ -0,0 +1,45 @@
+import LearnToPayAttention
+import numpy as np
+import scipy.io as sio
+from keras.datasets import cifar10
+from keras.datasets import cifar100
+
+
+if __name__ == "__main__":
+    
+    (x10, y10), (x_test, y_test) = cifar10.load_data()
+    (x100,y100),(x_test2,y_test2) = cifar100.load_data(label_mode='fine')
+    x10 = np.reshape(x10,[50000,32,32,3])
+    x100 = np.reshape(x100,[50000,32,32,3])
+    svhn_data = sio.loadmat('svhn.mat')
+    xsvhn = svhn_data['X']
+    ysvhn = svhn_data['y']
+    xsvhn = np.reshape(xsvhn,[73257,32,32,3])
+    ysvhn = np.squeeze(ysvhn)
+
+    #todo: normalize and color-normalize x10, x100, and xsvhn.
+    #todo: implement and crop cub-200-2011
+
+
+    vggatt1concatdpCIFAR10 = AttentionVGG(att='att1', gmode='concat', compatibilityfunction='dp', height=32, width=32, channels=3, outputclasses=10).StandardFit("cifar10",x10,y10)
+    vggatt1concatdpCIFAR100 = AttentionVGG(att='att1', gmode='concat', compatibilityfunction='dp', height=32, width=32, channels=3, outputclasses=100).StandardFit("cifar100",x100,y100)
+
+    vggatt1concatpcCIFAR10 = AttentionVGG(att='att1', gmode='concat', compatibilityfunction='pc', height=32, width=32, channels=3, outputclasses=100).StandardFit("cifar10",x10,y10)
+    vggatt1concatpcCIFAR100 = AttentionVGG(att='att1', gmode='concat', compatibilityfunction='pc', height=32, width=32, channels=3, outputclasses=100).StandardFit("cifar100",x100,y100)
+
+    vggatt2indepdpCIFAR10 = AttentionVGG(att='att2', gmode='indep', compatibilityfunction='dp', height=32, width=32, channels=3, outputclasses=10).StandardFit("cifar10",x10,y10)
+    vggatt2indepdpCIFAR100 = AttentionVGG(att='att2', gmode='indep', compatibilityfunction='dp', height=32, width=32, channels=3, outputclasses=100).StandardFit("cifar100",x100,y100)
+
+    vggatt2indeppcCIFAR10 = AttentionVGG(att='att2', gmode='indep', compatibilityfunction='pc', height=32, width=32, channels=3, outputclasses=10).StandardFit("cifar10",x10,y10)
+    vggatt2indeppcCIFAR100 = AttentionVGG(att='att2', gmode='indep', compatibilityfunction='pc', height=32, width=32, channels=3, outputclasses=100).StandardFit("cifar100",x100,y100)
+
+    vggatt2concatpcCIFAR10 = AttentionVGG(att='att2', gmode='concat', compatibilityfunction='pc', height=32, width=32, channels=3, outputclasses=10).StandardFit("cifar10",x10,y10)
+    vggatt2concatpcCIFAR100 = AttentionVGG(att='att2', gmode='concat', compatibilityfunction='pc', height=32, width=32, channels=3, outputclasses=100).StandardFit("cifar100",x100,y100)
+    vggatt2concatpcSVHN = AttentionVGG(att='att2', gmode='concat', compatibilityfunction='pc', height=32, width=32, channels=3, outputclasses=10).StandardFit("svhn",xsvhn,ysvhn)
+
+    vggatt3concatpcCIFAR10 = AttentionVGG(att='att3', gmode='concat', compatibilityfunction='pc', height=32, width=32, channels=3, outputclasses=10).StandardFit("cifar10",x10,y10)
+    vggatt3concatpcCIFAR100 = AttentionVGG(att='att3', gmode='concat', compatibilityfunction='pc', height=32, width=32, channels=3, outputclasses=100).StandardFit("cifar100",x100,y100)
+    vggatt3concatpcSVHN = AttentionVGG(att='att3', gmode='concat', compatibilityfunction='pc', height=32, width=32, channels=3, outputclasses=10).StandardFit("svhn",xsvhn,ysvhn)
+
+
+

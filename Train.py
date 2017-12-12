e77badcaed636bb2cbcfef00a4d5fa55d9fa3477
DadianisBidza <kutxisdaxra@outlook.com>
2017-12-13 02:49:57 +0400

    Fixed min_deltas.

--- Train.py
+++ Train.py
@@ -22,15 +22,15 @@ ycub = np.load("datasets/ycub.npy")
 ycubtest = np.load("datasets/ycubtest.npy")
 
 
-#vggatt3concatpcCIFAR10 = AttentionVGG(att='att3', gmode='concat', compatibilityfunction='pc', height=32, width=32, channels=3, outputclasses=10).StandardFit("cifar10",x10,y10,beep=True, min_delta=0.001, validation_data=(x10test,y10test))
-vggatt3concatpcCIFAR100 = AttentionVGG(att='att3', gmode='concat', compatibilityfunction='pc', height=32, width=32, channels=3, outputclasses=100).StandardFit("cifar100",x100,y100,beep=True, min_delta=0.001, validation_data=(x100test,y100test))
-vggatt3concatpcSVHN = AttentionVGG(att='att3', gmode='concat', compatibilityfunction='pc', height=32, width=32, channels=3, outputclasses=10).StandardFit("svhn",xsvhn,beep=True, min_delta=0.001, validation_data=(xsvhtest,ysvhntest))
+#vggatt3concatpcCIFAR10 = AttentionVGG(att='att3', gmode='concat', compatibilityfunction='pc', height=32, width=32, channels=3, outputclasses=10).StandardFit("cifar10",x10,y10,beep=True, min_delta=0.1, validation_data=(x10test,y10test))
+vggatt3concatpcCIFAR100 = AttentionVGG(att='att3', gmode='concat', compatibilityfunction='pc', height=32, width=32, channels=3, outputclasses=100).StandardFit("cifar100",x100,y100,beep=True, min_delta=0.1, validation_data=(x100test,y100test))
+vggatt3concatpcSVHN = AttentionVGG(att='att3', gmode='concat', compatibilityfunction='pc', height=32, width=32, channels=3, outputclasses=10).StandardFit("svhn",xsvhn,beep=True, min_delta=0.1, validation_data=(xsvhtest,ysvhntest))
 
-vggatt2concatpcCIFAR10 = AttentionVGG(att='att2', gmode='concat', compatibilityfunction='pc', height=32, width=32, channels=3, outputclasses=10).StandardFit("cifar10",x10,beep=True, min_delta=0.001, validation_data=(x10test,y10test))
-vggatt2concatpcCUB = AttentionVGG(att='att2', gmode='concat', compatibilityfunction='pc', height=32, width=32, channels=3, outputclasses=10).StandardFit("cub2002011",xcub,ycub,True,beep=True, min_delta=0.001, validation_data=(xcubtest,ycubtest))
+vggatt2concatpcCIFAR10 = AttentionVGG(att='att2', gmode='concat', compatibilityfunction='pc', height=32, width=32, channels=3, outputclasses=10).StandardFit("cifar10",x10,beep=True, min_delta=0.1, validation_data=(x10test,y10test))
+vggatt2concatpcCUB = AttentionVGG(att='att2', gmode='concat', compatibilityfunction='pc', height=32, width=32, channels=3, outputclasses=10).StandardFit("cub2002011",xcub,ycub,True,beep=True, min_delta=0.1, validation_data=(xcubtest,ycubtest))
 
-RNatt2concatpcCIFAR10 = AttentionRN(att='att2', gmode='concat', compatibilityfunction='pc', height=32, width=32, channels=3, outputclasses=10).StandardFit("cifar10",x10,y10,beep=True, min_delta=0.001, validation_data=(x10test,y10test))
-RNatt2concatpcCIFAR100 = AttentionRN(att='att2', gmode='concat', compatibilityfunction='pc', height=32, width=32, channels=3, outputclasses=100).StandardFit("cifar100",x100,y100,beep=True, min_delta=0.001, validation_data=(x100test,y100test))
+RNatt2concatpcCIFAR10 = AttentionRN(att='att2', gmode='concat', compatibilityfunction='pc', height=32, width=32, channels=3, outputclasses=10).StandardFit("cifar10",x10,y10,beep=True, min_delta=0.1, validation_data=(x10test,y10test))
+RNatt2concatpcCIFAR100 = AttentionRN(att='att2', gmode='concat', compatibilityfunction='pc', height=32, width=32, channels=3, outputclasses=100).StandardFit("cifar100",x100,y100,beep=True, min_delta=0.1, validation_data=(x100test,y100test))
     
 
 
